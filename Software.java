919c27f965d2203ae5b85871e69bb2ce1ee359ea
IIZODIACII <amr_x14@outlook.com>
2018-11-28 17:59:27 +0200

    Added A Function To Get The Median

--- Software.java
+++ Software.java
@@ -5,6 +5,8 @@
  */
 package software;
 
+import java.util.Arrays;
+
 /**
  *
  * @author note book
@@ -31,9 +33,21 @@ public class Software {
 		return avg;
 		
 	}
+
+	public static double GetMedian(int arr[]) {
+		Arrays.sort(arr);
+		double median;
+		
+		if (arr.length % 2 == 0)
+		    median = ((double)arr[arr.length/2] + (double)arr[arr.length/2 - 1])/2;
+		else
+		    median = (double) arr[arr.length/2];
+		
+		return median;
+	}
+
     public static void main(String[] args) {
         // TODO code application  logic here
-      
     }
     
 }
