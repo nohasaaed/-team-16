d5f623ab5f0027df140dfa9adcfaf4e1ddb9904e
unknown <alaaessam288@gmail.com>
2018-12-01 17:45:38 +0200

    LPN

--- Software.java
+++ Software.java
@@ -5,11 +5,41 @@
  */
 package software;
 
+
 /**
  *
  * @author note book
  */
 public class Software {
+	import java.util.Scanner;
+
+	// Java Program to find largest 
+		// prime factor of number 
+		
+		
+		public class Find_largest_prime_num {
+			public static int largestPrimeFactor(long number) {
+		        int i;
+
+		        for (i = 2; i <= number; i++) {
+		            if (number % i == 0) {
+		                number /= i;
+		                i--;
+		            }
+		        }
+
+		        return i;
+		    }
+			
+			public static void main(String[] args) {
+				Scanner input =new Scanner(System.in);
+			long n ;
+			n=input.nextLong();
+				System.out.println(Find_largest_prime_num.largestPrimeFactor(n));
+			}
+
+			
+		}
 
     /**
      * @param args the command line arguments
